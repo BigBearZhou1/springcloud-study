1ad1b3041f49d3dc1babd36a8f9dde5f918cd0c4
BigBearZhou1 <zhouwenzong@126.com>
2022-05-12 23:26:30 +0800

    test update username

--- cloud-config-center-3344/src/main/java/com/cloud/zwz/ConfigCenterMain3344.java
+++ cloud-config-center-3344/src/main/java/com/cloud/zwz/ConfigCenterMain3344.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 public class ConfigCenterMain3344 {
     public static void main(String[] args) {
+        System.out.println("hello");
         SpringApplication.run(ConfigCenterMain3344.class, args);
     }
 }
